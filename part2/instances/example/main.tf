55e357f68fdf2be3c05053ef88cc3636ec1f2596
Daniel Engbert <end1@umbc.edu>
2023-07-24 23:05:43 -0400

    wip solution to part2

--- part2/instances/example/main.tf
+++ part2/instances/example/main.tf
@@ -7,9 +7,22 @@ terraform {
   }
 }
 
+provider "aws" {
+  profile = "dangbert"
+  region  = "us-east-2"
+}
+
 module "website" {
-    source = "../../modules/aws-s3-static-website-bucket"
+  source = "../../modules/aws-s3-static-website-bucket"
 
-    # add more fields below to properly instantiate this module (based on the module's variables.tf file):
+  # add more fields below to properly instantiate this module (based on the module's variables.tf file):
 
+  # make this something unique to your AWS account:
+  bucket_name = "tf-tutorial-bucket"
+
+  # example tags, set to whatever you want or omit:
+  tags = {
+    "Terraform" = "true"
+    "ENV"       = "DEV"
+  }
 }
